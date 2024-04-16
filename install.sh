1bf1df62af873982eedc5f19f7c7c2f54f80acbe
djh20 <danmanmrdj@gmail.com>
2024-04-17 00:23:30 +1200

    Install `redis-server` package instead of `redis`

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyr
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 
 apt-get update
-apt-get install -y redis
+apt-get install -y redis-server
 
 cd /tmp
 
