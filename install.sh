c4b7b1641039fddf149805f6048eae60fc47a4c7
djh20 <danmanmrdj@gmail.com>
2024-04-17 14:45:55 +1200

    Fix redis dependency issue

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # Usage: curl https://raw.githubusercontent.com/djh20/volt/main/install.sh | sudo bash
 
-curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-
-apt-get update
-apt-get install -y redis-server
-
 cd /tmp
 
 curl -s https://api.github.com/repos/djh20/volt/releases/latest \
