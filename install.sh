cb3fd1497fdf74d381e71404a875b5ac8f026aba
djh20 <danmanmrdj@gmail.com>
2024-04-17 15:40:55 +1200

    Add ability to install beta builds

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-# Usage: curl https://raw.githubusercontent.com/djh20/volt/main/install.sh | sudo bash
+
+## Usage ##
+# Download Latest Stable Build:
+# curl -s https://raw.githubusercontent.com/djh20/volt/main/install.sh | sudo bash -s stable
+
+# Download Latest Beta Build: 
+# curl -s https://raw.githubusercontent.com/djh20/volt/main/install.sh | sudo bash -s beta
 
 cd /tmp
 
-curl -s https://api.github.com/repos/djh20/volt/releases/latest \
+URL="https://api.github.com/repos/djh20/volt/releases/latest"
+RELEASE_TYPE="$1"
+
+if [ "$RELEASE_TYPE" == "beta" ]; then
+  URL="https://api.github.com/repos/djh20/volt/releases?per_page=1"
+fi
+
+curl -s "$URL" \
 | grep "browser_download_url.*deb" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -O volt-latest.deb -qi -
+| wget -O volt.deb -qi -
 
-apt-get install ./volt-latest.deb
-rm volt-latest.deb
+apt-get install -y ./volt.deb
+rm volt.deb
